f13a666ce853847beda2fa971ac208126c72a251
Sophie Larsen <roxyknit2000@gmail.com>
2024-11-23 10:09:26 -0700

    Tested that webservice is running using curl

--- service/index.js
+++ service/index.js
@@ -1,114 +1,20 @@
+//imports cookie-parser from node
+const cookieParser = require('cookie-parser');
+//for encrypting
+const bcrypt = require('bcrypt');
+//need express to actually use backend and backend debugger
 const express = require('express');
-// lets us represent tokens with unique ids
-const uuid = require('uuid');
 const app = express();
 
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
-
-let users = {};
-// I think this has to be brackets because it will be in json format.
-let bookName = {bookName: ""};
-let otherBookNames = [{otherBookName: "Lizzie's Book"}, {otherBookName: "Elinor's Book"}, {otherBookName: "Anne's Book"}, {otherBookName: "Marianne's Book"}];
-
-app.use(express.json());
-
-app.use(express.static('public'));
-
-
-var apiRouter = express.Router();
-app.use(`/api`, apiRouter);
-
-
-// CreateAuth a new user
-apiRouter.post('/auth/create', async (req, res) => {
-  const user = users[req.body.email];
-  if (user) {
-    res.status(409).send({ msg: 'Existing user' });
-  } else {
-    const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
-    users[user.email] = user;
-
-    res.send({ token: user.token });
-  }
+app.post('/auth/create', async (req, res) => {
+  res.send({ id: 'user@id.com' });
 });
 
-
-// GetAuth login an existing user
-apiRouter.post('/auth/login', async (req, res) => {
-  const user = users[req.body.email];
-  if (user) {
-    if (req.body.password === user.password) {
-      user.token = uuid.v4();
-      res.send({ token: user.token });
-      return;
-    }
-  }
-  res.status(401).send({ msg: 'Unauthorized' });
+app.post('/auth/login', async (req, res) => {
+  res.send({ id: 'user@id.com' });
 });
 
-
-// DeleteAuth logout a user
-apiRouter.delete('/auth/logout', (req, res) => {
-  const user = Object.values(users).find((u) => u.token === req.body.token);
-  if (user) {
-    delete user.token;
-  }
-  res.status(204).end();
-});
-
-
-//get bookname
-apiRouter.get('/bookName', (_req, res) => {
-  res.send(bookName);
-});
-
-
-// Submit bookName
-apiRouter.post('/bookName', (req, res) => {
-  console.log(req.body)
-  bookName.bookName=req.body.bookName 
-  // updateBookName(req.body, bookName);
-  res.send(bookName);
-});
-
-
-function updateBookName(newBookName, bookName) {
-  bookName.bookName = newBookName;
-  return bookName;
-}
-
-apiRouter.delete('/auth/logout', (req, res) => {
-  const user = Object.values(users).find((u) => u.token === req.body.token);
-  if (user) {
-    delete user.token;
-  }
-  res.status(204).end();
-});
-
-
-apiRouter.get('/otherBookNames', (_req, res) => {
-  res.send(otherBookNames);
-});
-
-apiRouter.post('/otherBookName', (req, res) => {
-  scores = updateOtherBookNames(req.body, otherBookNames);
-  res.send(otherBookNames);
-});
-
-
-//For right now, this is a placeholder.
-// function updateScores(newScore, scores) {
-//   let found = false;
-//   for (const [i, prevScore] of scores.entries()) {
-//     if (newScore.score > prevScore.score) {
-//       scores.splice(i, 0, newScore);
-//       found = true;
-//       break;
-//     }
-//   }
-
-
-
-app.listen(port, () => {
+const port = 4000;
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
