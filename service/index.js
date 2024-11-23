ac12ed9c6192a826d88adfc8bbf34f0392a9bfec
Sophie Larsen <roxyknit2000@gmail.com>
2024-11-23 11:07:24 -0700

    Added in some of the index.js code for authentication in the login

--- service/index.js
+++ service/index.js
@@ -5,16 +5,84 @@ const bcrypt = require('bcrypt');
 //need express to actually use backend and backend debugger
 const express = require('express');
 const app = express();
+const DB = require('./database.js');
 
-app.post('/auth/create', async (req, res) => {
-  res.send({ id: 'user@id.com' });
+const authCookieName = 'token';
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+//APP.USE: basically allows us to use features in the request/response cycle. That's what middleware is. I understand it better now.
+//allows us to convert data into json, which is more easily usable.
+app.use(express.json());
+//lets us use cookie-parser
+app.use(cookieParser());
+
+app.use(express.static('public'));
+
+app.set('trust proxy', true);
+
+//makes an express router instance
+const apiRouter = express.Router();
+//makes it so all routes in apiRouter will go through path /api
+app.use('/api', apiRouter);
+
+
+//------------------------------------------------------------------------------------------------------------------------------------
+// ENDPOINTS
+//------------------------------------------------------------------------------------------------------------------------------------
+//will go through /api, as an endpoint under apiRouter.
+
+//post: client sends in data, and we respond accordingly.
+
+//create user token if not already in system
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await DB.getUser(req.body.email)) {
+    res.status(409).send({ msg: 'User already exists' });
+  }
+  else {
+    //calls createUser, which will be defined in database.js, since it is adding to the database
+    const user = await DB.createUser(req.body.email, req.body.password);
+    
+    //set cookie. defined much later.
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
+  }
 });
 
-app.post('/auth/login', async (req, res) => {
-  res.send({ id: 'user@id.com' });
+//get token for credentials when logging in
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+})
+
+
+//delete Auth token if logging out
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
 });
 
-const port = 4000;
-app.listen(port, function () {
-  console.log(`Listening on port ${port}`);
+//verify credentials for endpoints
+const secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  const authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  }
+  else {
+    res.status(401).send({ msg: 'Unauthorized'});
+  }
 });
